00d9e8169682e0d17c6254ee40507f8ca1dace9a
emanueldias01 <emanuelaraujo90@gmail.com>
2024-03-11 21:23:28 -0300

    cirriação do controller de Food

---
+++ food-api/food-api/src/main/java/com/api/food/foodapi/controller/FoodController.java
@@ -0,0 +1,47 @@
+package com.api.food.foodapi.controller;
+
+import com.api.food.foodapi.dto.FoodRequestDTO;
+import com.api.food.foodapi.dto.FoodUpdateDTO;
+import com.api.food.foodapi.service.FoodService;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+import org.springframework.web.util.UriComponentsBuilder;
+
+@RestController
+@RequestMapping("/foods")
+public class FoodController {
+
+    @Autowired
+    FoodService service;
+
+
+    @GetMapping
+    public ResponseEntity getAll(){
+        return service.serviceGetAllFoods();
+
+    }
+
+    @PostMapping
+    @Transactional
+    public ResponseEntity postFood(@RequestBody FoodRequestDTO data, UriComponentsBuilder uriBuilder){
+        return service.serviceSaveFood(data, uriBuilder);
+
+    }
+
+    @PutMapping
+    @Transactional
+    public ResponseEntity putFood(@RequestBody FoodUpdateDTO data){
+        return service.serviceUpdateFood(data);
+
+    }
+
+    @DeleteMapping("/{id}")
+    @Transactional
+    public ResponseEntity deleteFood(@PathVariable String id){
+        return service.serviceDeleteFood(id);
+
+    }
+
+}
